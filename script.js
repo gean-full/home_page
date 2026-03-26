340640bbb315e887a8bbd6597e8ad46581379d5f
gean-full <geanvacaro2054@gmail.com>
2026-03-26 16:18:40 -0500

    Finalized

---
+++ script.js
@@ -0,0 +1,8 @@
+function comprar() {
+  alert("Produto adicionado ao carrinho!");
+}
+const produtos = [
+  { nome: "Fone", preco: 199 },
+  { nome: "Mouse", preco: 99 },
+  { nome: "Teclado", preco: 150 }
+];
